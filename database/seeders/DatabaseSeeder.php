a794aeeb63fa8110f9fbe97f2b080306250b4bb3
bilkis0987 <2425286.bilkis@smkn-2sbg.sch.id>
2026-06-28 08:00:00 +0700

    Create DatabaseSeeder base class

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call([
+            CategorySeeder::class,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin Desa',
+            'email' => 'admin@desa.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
